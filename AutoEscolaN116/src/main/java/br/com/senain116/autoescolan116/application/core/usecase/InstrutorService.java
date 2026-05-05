b01850100bb5c99f17f07ef5e8179cdaabc94392
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 16:54:40 -0300

    feat: implement InstrutorService for instructor management

---
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/core/usecase/InstrutorService.java
@@ -0,0 +1,60 @@
+package br.com.senain116.autoescolan116.application.core.usecase;
+
+import br.com.senain116.autoescolan116.adapter.in.controller.mapper.EnderecoMapper;
+import br.com.senain116.autoescolan116.adapter.in.controller.mapper.InstrutorMapper;
+import br.com.senain116.autoescolan116.adapter.in.controller.request.instrutor.DadosAtualizacaoInstrutor;
+import br.com.senain116.autoescolan116.adapter.in.controller.request.instrutor.DadosCadastroInstrutor;
+import br.com.senain116.autoescolan116.adapter.in.controller.response.instrutor.DadosDetalhamentoInstrutor;
+import br.com.senain116.autoescolan116.application.core.domain.model.Instrutor;
+import br.com.senain116.autoescolan116.application.port.out.InstrutorRepository;
+import br.com.senain116.autoescolan116.exception.type.instrutor.InstrutorNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstrutorService {
+    private final InstrutorRepository repository;
+    private final InstrutorMapper mapper;
+    private final EnderecoMapper enderecoMapper;
+
+    public InstrutorService(InstrutorRepository repository,InstrutorMapper mapper, EnderecoMapper enderecoMapper){
+        this.repository = repository;
+        this.mapper = mapper;
+        this.enderecoMapper = enderecoMapper;
+    }
+
+    public DadosDetalhamentoInstrutor cadastrar(DadosCadastroInstrutor dados){
+        Instrutor instrutor = mapper.toDomain(dados);
+        Instrutor saved = repository.save(instrutor);
+        return mapper.toDetailsDTO(saved);
+    }
+
+   public Page listar(Pageable paginacao){
+        return repository
+                .findAllByAtivoTrue(paginacao)
+                .map(mapper::toListDTO);
+   }
+
+   public DadosDetalhamentoInstrutor detalhar(Long id){
+        Instrutor instrutor = repository.findById(id)
+                .orElseThrow(() -> new InstrutorNotFoundException("ID do instrutor nao encontrado: " + id));
+        return mapper.toDetailsDTO(instrutor);
+   }
+
+   @Transactional
+    public  atualizar(DadosAtualizacaoInstrutor dados){
+        Instrutor instrutor = repository.findById(dados.id())
+                .orElseThrow(() -> new InstrutorNotFoundException("ID do instrutor nao encontrado: " + dados.id()));
+        instrutor.atualizarInformacoes(
+                dados.nome(),
+                dados.email(),
+                dados.telefone(),
+                dados.especialidade(),
+                enderecoMapper.toDomain(dados.endereco())
+        );
+        Instrutor saved = repository.save(instrutor);
+        return mapper.toDetailsDTO(saved);
+   }
+}

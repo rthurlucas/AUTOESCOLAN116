ce7a1ad8feed5c1f6efd3184330223a69f005f4f
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 16:54:27 -0300

    feat: enhance InstrutorMapper with new DTO conversion methods

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/mapper/InstrutorMapper.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/mapper/InstrutorMapper.java
@@ -1,8 +1,12 @@
 package br.com.senain116.autoescolan116.adapter.in.controller.mapper;
 
 import br.com.senain116.autoescolan116.adapter.in.controller.request.instrutor.DadosCadastroInstrutor;
+import br.com.senain116.autoescolan116.adapter.in.controller.response.instrutor.DadosDetalhamentoInstrutor;
+import br.com.senain116.autoescolan116.adapter.in.controller.response.instrutor.DadosListagemInstrutor;
 import br.com.senain116.autoescolan116.application.core.domain.model.Instrutor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InstrutorMapper {
     private final EnderecoMapper enderecoMapper;
 
@@ -11,6 +15,37 @@ public class InstrutorMapper {
     }
 
     public Instrutor toDomain(DadosCadastroInstrutor dados){
+        return new Instrutor(
+                null,
+                dados.nome(),
+                dados.email(),
+                dados.telefone(),
+                dados.cnh(),
+                dados.especialidade(),
+                enderecoMapper.toDomain(dados.endereco()),
+                true
+        );
+    }
 
+    public DadosListagemInstrutor toListDTO(Instrutor instrutor){
+        return new DadosListagemInstrutor(
+                instrutor.getId(),
+                instrutor.getNome(),
+                instrutor.getEmail(),
+                instrutor.getEspecialidade()
+        );
+    }
+
+    public DadosDetalhamentoInstrutor toDetailsDTO(Instrutor instrutor){
+        return new DadosListagemInstrutor(
+                instrutor.getId(),
+                instrutor.getNome(),
+                instrutor.getEmail(),
+                instrutor.getTelefone(),
+                instrutor.getCnh(),
+                instrutor.getEspecialidade(),
+                enderecoMapper.toDTO(instrutor.getEndereco()),
+                instrutor.isAtivo()
+        );
     }
 }

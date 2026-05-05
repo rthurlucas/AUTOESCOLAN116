c73bffabd1675f0e8c4226ec5dfe408daa5260b7
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 20:25:36 -0300

    refactor: Organizando toda a regra de negocio para o Service do Instrutor

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/InstrutorController.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/InstrutorController.java
@@ -5,11 +5,11 @@ import br.com.senain116.autoescolan116.adapter.in.controller.request.instrutor.D
 import br.com.senain116.autoescolan116.adapter.in.controller.response.instrutor.DadosDetalhamentoInstrutor;
 import br.com.senain116.autoescolan116.adapter.in.controller.response.instrutor.DadosListagemInstrutor;
 import br.com.senain116.autoescolan116.application.core.domain.model.Instrutor;
+import br.com.senain116.autoescolan116.application.core.usecase.InstrutorService;
+import br.com.senain116.autoescolan116.application.port.in.ModelDomainController;
 import br.com.senain116.autoescolan116.application.port.out.InstrutorRepository;
-import br.com.senain116.autoescolan116.domain.instrutor.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -22,64 +22,62 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/instrutores")
-public class InstrutorController {
-    @Autowired
-    private InstrutorRepository repository;
+public class InstrutorController implements ModelDomainController<
+        DadosCadastroInstrutor,
+        DadosListagemInstrutor,
+        DadosAtualizacaoInstrutor,
+        Void,
+        DadosDetalhamentoInstrutor,
+        Long> {
+        private InstrutorRepository repository;
+        private InstrutorService service;
 
+        public InstrutorController(InstrutorRepository repository, InstrutorService service) {
+            this.repository = repository;
+            this.service = service;
+        }
+
+    @Override
     @PostMapping
-    @Transactional
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    public ResponseEntity<DadosDetalhamentoInstrutor> cadastrarInstrutor(
-            @RequestBody @Valid DadosCadastroInstrutor dados,
-            UriComponentsBuilder uriBuilder) {
-        Instrutor instrutor = new Instrutor(dados);
-        repository.save(instrutor);
+    public ResponseEntity<DadosDetalhamentoInstrutor> cadastrar(
+            DadosCadastroInstrutor dados, UriComponentsBuilder uriBuilder) {
+        DadosDetalhamentoInstrutor dto = service.cadastrar(dados);
         URI uri = uriBuilder
                 .path("/instrutores/{id}")
-                .buildAndExpand(instrutor.getId())
+                .buildAndExpand(dto.id())
                 .toUri();
-        DadosDetalhamentoInstrutor dto = new DadosDetalhamentoInstrutor(instrutor);
         return ResponseEntity.created(uri).body(dto);
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    public ResponseEntity<Page<DadosListagemInstrutor>> listarInstrutores(
+        @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+        public ResponseEntity<Page<DadosListagemInstrutor>> listar(
             @PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
-        Page page = repository
-                .findAllByAtivoTrue(paginacao)
-                .map(DadosListagemInstrutor::new);
-        return ResponseEntity.ok(page);
+        return ResponseEntity.ok(service.listar(paginacao));
     }
 
-    @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    public ResponseEntity<DadosDetalhamentoInstrutor> detalharInstrutor(
+        @GetMapping("/{id}")
+        @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+        public ResponseEntity<DadosDetalhamentoInstrutor> detalhar(
             @PathVariable Long id) {
         Instrutor instrutor = repository.getReferenceById(id);
-        DadosDetalhamentoInstrutor dto = new DadosDetalhamentoInstrutor(instrutor);
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.detalhar(id));
     }
 
-    @PutMapping
-    @Transactional
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    public ResponseEntity<DadosDetalhamentoInstrutor> atualizarInstrutor(
+        @PutMapping
+        @Transactional
+        @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+        public ResponseEntity<DadosDetalhamentoInstrutor> atualizar(
             @RequestBody @Valid DadosAtualizacaoInstrutor dados) {
-        Instrutor instrutor = repository.getReferenceById(dados.id());
-        instrutor.atualizarInformacoes(dados);
-        repository.save(instrutor);
-        DadosDetalhamentoInstrutor dto = new DadosDetalhamentoInstrutor(instrutor);
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.atualizar(dados));
     }
 
-    @DeleteMapping("/{id}")
-    @Transactional
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
-    public ResponseEntity<Void> excluirInstrutor(@PathVariable Long id) {
-        Instrutor instrutor = repository.getReferenceById(id);
-        instrutor.excluir();
-        repository.save(instrutor);
+        @DeleteMapping("/{id}")
+        @Transactional
+        @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+        public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        service.excluir(id);
         return ResponseEntity.noContent().build();
     }
 }

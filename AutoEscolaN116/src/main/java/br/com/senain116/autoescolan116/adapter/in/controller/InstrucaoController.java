b084af324aea93badf7c59a913c213d438bb7c70
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 21:43:49 -0300

    fix: Corrigindo bug na regra de negocio do InstrucaoController

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/InstrucaoController.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/in/controller/InstrucaoController.java
@@ -1,25 +1,37 @@
 package br.com.senain116.autoescolan116.adapter.in.controller;
 
+import br.com.senain116.autoescolan116.adapter.in.controller.response.instrucao.DadosListagemInstrucao;
 import br.com.senain116.autoescolan116.application.core.usecase.AgendaDeInstrucoes;
 import br.com.senain116.autoescolan116.adapter.in.controller.request.instrucao.DadosAgendamento;
 import br.com.senain116.autoescolan116.adapter.in.controller.response.instrucao.DadosDetalhamentoAgendamento;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/instrucoes")
 public class InstrucaoController {
-    @Autowired
+
     private AgendaDeInstrucoes agenda;
+
+    public InstrucaoController(AgendaDeInstrucoes agenda){
+        this.agenda = agenda;
+    }
 
     @PostMapping
     public ResponseEntity<DadosDetalhamentoAgendamento> agendarInstrucao(
             @RequestBody @Valid DadosAgendamento dados) {
         return ResponseEntity.ok(agenda.agendar(dados));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public Page<DadosListagemInstrucao> listagemInstrucao(@PageableDefault(size = 10, sort = "name")Pageable paginacao){
+        return ResponseEntity.ok(agenda.listar(paginacao)).getBody();
     }
 }

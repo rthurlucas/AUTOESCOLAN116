2fcac101e5455b4099cf6dfd84ddc28a5fd6ae7c
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 21:44:08 -0300

    feat: Instrucao criada

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/core/domain/model/Instrucao.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/core/domain/model/Instrucao.java
@@ -1,30 +1,36 @@
 package br.com.senain116.autoescolan116.application.core.domain.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@Entity(name = "Instrucao")
-@Table(name = "instrucoes")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@EqualsAndHashCode(of = "id")
 public class Instrucao {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "instrutor_id")
     private Instrutor instrutor;
     private LocalDateTime data;
+
+    public Instrucao(Long id, Aluno aluno, Instrutor instrutor, LocalDateTime data) {
+        this.id = id;
+        this.aluno = aluno;
+        this.instrutor = instrutor;
+        this.data = data;
+    }
+
+    public Instrucao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public Instrutor getInstrutor() {
+        return instrutor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
 }

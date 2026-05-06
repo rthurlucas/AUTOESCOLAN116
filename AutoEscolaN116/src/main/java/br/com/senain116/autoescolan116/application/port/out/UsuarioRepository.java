ee0f61832f9e0b4f80b453df5b5d9c11d854f8da
rthurlucas <arthurlucasx696@gmail.com>
2026-05-05 21:40:09 -0300

    refactor: removendo o existsByIdAndAtivoTrue para outro repository

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/port/out/UsuarioRepository.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/port/out/UsuarioRepository.java
@@ -6,8 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository {
     UserDetails findByLogin(String username);
 
     Page<Usuario> findAllByAtivoTrue(Pageable paginacao);
+
+    boolean existsByIdAndAtivoTrue(Long id);
+
+    Usuario save(Usuario usuario);
+
+    Usuario findById(Long id);
 }

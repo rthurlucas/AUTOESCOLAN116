f65c13efaae35a10b6445758ebbdeb8209677d99
rthurlucas <arthurlucasx696@gmail.com>
2026-05-08 20:39:26 -0300

    feat: Implementando os jpa para o repository entender a logica

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/out/repository/persistence/InstrucaoJpaRepository.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/adapter/out/repository/persistence/InstrucaoJpaRepository.java
@@ -1,11 +1,16 @@
 package br.com.senain116.autoescolan116.adapter.out.repository.persistence;
 
 import br.com.senain116.autoescolan116.adapter.out.repository.entity.InstrucaoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface InstrucaoJpaRepository extends JpaRepository<InstrucaoEntity, Long> {
     boolean existsByAlunoIdAndDataBetween(Long id, LocalDateTime inicio, LocalDateTime fim);
     boolean existsByInstrutorIdAndData(Long id, LocalDateTime data);
+    Page<InstrucaoEntity> findAll(Pageable paginacao);
+    Optional<InstrucaoEntity> findById(Long id);
 }
--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/port/out/InstrucaoRepository.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/port/out/InstrucaoRepository.java
@@ -1,7 +1,7 @@
 package br.com.senain116.autoescolan116.application.port.out;
 
 import br.com.senain116.autoescolan116.application.core.domain.model.Instrucao;
-import org.apache.el.stream.Stream;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
@@ -9,11 +9,9 @@ import java.util.Optional;
 
 public interface InstrucaoRepository{
     boolean existsByAlunoIdAndDataBetween(Long id, LocalDateTime inicio, LocalDateTime fim);
-
     boolean existsByInstrutorIdAndData(Long id, LocalDateTime data);
-
     Instrucao save(Instrucao instrucao);
     Optional<Instrucao> findById(Long id);
-
-    Stream findAll(Pageable paginacao);
+    Page<Instrucao> findAll(Pageable paginacao);
+    void delete(Instrucao agenda);
 }

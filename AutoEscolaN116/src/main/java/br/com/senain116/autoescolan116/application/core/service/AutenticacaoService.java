3e8457ce15d9eb9a3cc574c8173a6652f5fabb70
rthurlucas <arthulucasx696@gmail.com>
2026-05-11 16:29:39 -0300

    fix: corrigindo a logica do loadUserByUsername

--- AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/core/service/AutenticacaoService.java
+++ AutoEscolaN116/src/main/java/br/com/senain116/autoescolan116/application/core/service/AutenticacaoService.java
@@ -1,11 +1,15 @@
 package br.com.senain116.autoescolan116.application.core.service;
 
+import br.com.senain116.autoescolan116.adapter.out.repository.entity.UsuarioEntity;
+import br.com.senain116.autoescolan116.adapter.out.repository.persistence.UsuarioJpaRepository;
 import br.com.senain116.autoescolan116.application.port.out.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
@@ -16,7 +20,7 @@ public class AutenticacaoService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByLogin(username);
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return repository.findByLogin(login);
     }
 }
